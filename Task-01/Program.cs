4fd36bbd84f8ef87a5ad757826735625b87f9354
Vladimirs77nt <vladimirs77@yandex.ru>
2022-10-08 09:41:47 +0500

    задача 1 - убрал лишние строки, почистил

--- Task-01/Program.cs
+++ Task-01/Program.cs
@@ -1,39 +1,16 @@
-﻿// 3. Напишите программу, которая будет выдавать
-// название дня недели по заданному номеру.
-// 3 -> Среда
-// 5 -> Пятница
+﻿// 1. Напишите программу, которая на вход принимает два
+// числа и проверяет, является ли первое число квадратом
+// второго.
+// a = 25, b = 5 -> да
+// a = 2, b = 10 -> нет
+// a = 9, b = -3 -> да
+// a = -3 b = 9 -> нет
 
-Console.Write("Введите день недели: ");
-int day = Convert.ToInt32(Console.ReadLine());
-if (day == 1)
-{
-    Console.Write($"{day} -> ПОНЕДЕЛЬНИК");
-}
-if (day == 2)
-{
-    Console.Write($"{day} -> ВТОРНИК");
-}
-if (day == 3)
-{
-    Console.Write($"{day} -> СРЕДА");
-}
-if (day == 4)
-{
-    Console.Write($"{day} -> ЧЕТВЕРГ");
-}
-if (day == 5)
-{
-    Console.Write($"{day} -> ПЯТНИЦА");
-}
-if (day == 6)
-{
-    Console.Write($"{day} -> СУББОТА");
-}
-if (day == 7)
-{
-    Console.Write($"{day} -> ВОСКРЕСЕНЬЕ");
-}
-if (day > 7)
-{
-    Console.Write($"{day} -> такого дня недели нет");
-}
+Console.Write("Введите первое число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе число: ");
+int b = Convert.ToInt32(Console.ReadLine());
+if (a == b*b)
+    Console.WriteLine($"Да, число {a} является квадратом числа {b}");
+else
+    Console.WriteLine($"Нет, число {a} НЕ является квадратом числа {b}");
