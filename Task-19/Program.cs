182a68a6e8cbeb4c3e5ae82a08a2098d87ed4afe
Vladimirs77nt <vladimirs77@yandex.ru>
2022-10-08 08:31:09 +0500

    Задача 19 решена с использованием процедуры

--- Task-19/Program.cs
+++ Task-19/Program.cs
@@ -8,15 +8,18 @@
 Console.Write("Введите пятизначное число: ");
 int number = Convert.ToInt32(Console.ReadLine());
 if (number > 9999 && number < 100000)
-    {
-    int digit5 = number % 10; // 12345 % 10 = 5
-    int digit4 = number/10 % 10; // 1234 % 10 = 4
-    int digit3 = number/100 % 10; // 123 % 10 = 3
-    int digit2 = number/1000 % 10; // 12 % 10 = 2
-    int digit1 = number/10000; // 1 = 1
-    int palinodrom = digit5*10000+digit4*1000+digit3*100+digit2*10+digit1;
-    if (number == palinodrom)
+    if (number == Palinodrom(number))
         Console.WriteLine($"{number} -> да");
-    else Console.WriteLine("нет");
-    }
-else Console.WriteLine ("ошибка, введено неверное число!");
+    else Console.WriteLine($"{number} -> нет");
+else Console.WriteLine ("ошибка, введено неверное число");
+
+int Palinodrom (int num)
+    {
+    int digit5 = num % 10; // 12345 % 10 = 5
+    int digit4 = num/10 % 10; // 1234 % 10 = 4
+    int digit3 = num/100 % 10; // 123 % 10 = 3
+    int digit2 = num/1000 % 10; // 12 % 10 = 2
+    int digit1 = num/10000; // 1 = 1
+    int palinodrom = digit5*10000+digit4*1000+digit3*100+digit2*10+digit1;
+    return palinodrom;
+    };
