c6463bc6509a3f3979269c7f4eba6e1bb0cfa64d
Vladimirs77nt <vladimirs77@yandex.ru>
2022-10-14 01:58:17 +0500

    Задача 25 - добавлены комментарии

--- Task-25/Program.cs
+++ Task-25/Program.cs
@@ -11,20 +11,20 @@ int num_B = Convert.ToInt32(Console.ReadLine());
 int num_Degree = Degree(num_A, num_B);
 Console.WriteLine($"{num_A}, {num_B} -> {num_Degree}");
 
-int Degree(int a, int b)
+int Degree(int a, int b) // метод возведения в степень
 {
     int result = 1;
-    if (b < 0)
+    if (b < 0) // проверка на отрицательные числа
         {
         Console.WriteLine("Ошибка ввода степени!");
         result = 0;
         }
-    else if (b == 0) Console.WriteLine ("Любое число в степени 0 равно 1");
+    else if (b == 0) Console.WriteLine ("Любое число в степени 0 равно 1"); // проверка на степень 1
     else
     {
         for (int i = 0; i < b; i++)
         {
-        result = result * a;
+        result = result * a; // возведение в степень...
         }
     }
     return result;
