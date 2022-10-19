0ec90d5d9b82266182306b940351d5d35bd0c1a4
Vladimirs77nt <vladimirs77@yandex.ru>
2022-10-19 21:56:47 +0500

    Задача 43 решена! скорректирована

--- Task-43/Program.cs
+++ Task-43/Program.cs
@@ -3,29 +3,35 @@
 // значения b1, k1, b2 и k2 задаются пользователем.
 // b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
 
-double IntersectionPointX (double b1, double k1, double b2, double k2)
+double IntersectionPointX (double b1, double k1, double b2, double k2) // методы вычисления Y по заданным уравнениям
 {
-    double x = (b2 - b1) / (k1 - k2);
-    return x;
+    double xpoint = (b2 - b1) / (k1 - k2);
+    return xpoint;
 }
 
+Console.WriteLine();                                  // начало основной программы
 Console.WriteLine("Вычисление точки пересечения двух прямых, заданных уравнениями:");
 Console.WriteLine("y = k1 * x + b1");
 Console.WriteLine("y = k2 * x + b2");
 Console.WriteLine();
 
-Console.Write("Введите значение b1: ");
+Console.Write("Введите значение b1: ");              // консольный ввод данных
 double b1 = Convert.ToDouble(Console.ReadLine());
-
 Console.Write("Введите значение k1: ");
 double k1 = Convert.ToDouble(Console.ReadLine());
-
 Console.Write("Введите значение b2: ");
 double b2 = Convert.ToDouble(Console.ReadLine());
-
 Console.Write("Введите значение k2: ");
 double k2 = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine();
 
-double xpoint = IntersectionPointX(b1, k1, b2, k2);
-double ypoint = k1 * xpoint + b1;
-Console.WriteLine($"{xpoint}, {ypoint}");
+if ( k1 == k2)                                  // проверка коэффициентов k на одинаковое значение
+{                                               // если они одинаковые - то линии параллельные, т.е. не пересекаются
+    Console.WriteLine("Линии с заданными параметрами не пересекаются");
+}
+else
+{                                                         // поиск точки пересечения
+    double x = IntersectionPointX(b1, k1, b2, k2);   // вызов метода вычисления координаты Y (точка пересечения)
+    double y = k1 * x + b1;                     // вычисление координаты X (точка пересечения)
+    Console.WriteLine($"Координаты точки пересечения: x = {x}, y = {y}");
+}
