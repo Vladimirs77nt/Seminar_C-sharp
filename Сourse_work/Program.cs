0c8db312d63f84a685ca7626410250b22759a9c0
Vladimirs77nt <vladimirs77@yandex.ru>
2022-11-26 13:06:47 +0500

    Программа исправлена. Внесены мелкие корректировки

--- Сourse_work/Program.cs
+++ Сourse_work/Program.cs
@@ -3,47 +3,53 @@
 // выполнения алгоритма. При решении не рекомендуется пользоваться коллекциями, лучше обойтись исключительно
 // массивами.
 
+// Вариант 1 - без использования методов
 
 // 1 часть - консольный ввод данных (создание первого массива)
-Console.WriteLine("Требуется ввести некоторое кол-во строк различной длины/");
-Console.WriteLine("Сначала введите кол-во вводимых строк, затем начните вводить строки завершая их нажатием клавиши Enter");
-Console.WriteLine("Введите количество вводимых строк: ");
+Console.WriteLine("Требуется ввести некоторое кол-во строк различной длины.");
+Console.WriteLine("Сначала введите кол-во вводимых строк:");
 int sizeArray = Convert.ToInt32(Console.ReadLine());
-var StringArray = new string[sizeArray];
-for (int i = 0; i < sizeArray; i++)
-{
-    Console.Write($"введите строку {i+1}: ");
-    StringArray[i] = Console.ReadLine();
-}
 
-// 2 часть - вычисление: сколько введенных строк удовлетворяют условию задачи
-int sizeArray2 = 0;
-for (int i = 0; i < sizeArray; i++)
+if (sizeArray > 0)  // проверка условия необходимости дальнейшего решения задачи
 {
-    if (StringArray[i].Length <= 3) sizeArray2++;
-}
-
-if (sizeArray2 != 0)         // проверка условия необходимости дальнейшего решения задачи
-{
-// 3 часть - создание второго массива со строками удовлетворяющими условиям задачи
-int count = 0;
-var StringArray2 = new string[sizeArray2];     // создаем второй массив
-for (int i = 0; i < sizeArray; i++)
-{
-    if (StringArray[i].Length <= 3)
+    Console.WriteLine("Начните вводить строки, завершая их нажатием клавиши Enter");
+    var StringArray = new string[sizeArray];
+    for (int i = 0; i < sizeArray; i++)
     {
-        StringArray2[count] = StringArray[i];
-        count++;
+        Console.Write($"введите строку {i + 1}: ");
+        StringArray[i] = Console.ReadLine();
     }
-}
 
-// 4 часть - вывод результата
-Console.Write("Результат: ");
-for (int i = 0; i < sizeArray2-1; i++)
-{
-    Console.Write(StringArray2[i]);
-    Console.Write(", ");
+    // 2 часть - вычисление: сколько введенных строк удовлетворяют условию задачи
+    int sizeArray2 = 0;
+    for (int i = 0; i < sizeArray; i++)
+    {
+        if (StringArray[i].Length <= 3) sizeArray2++;
+    }
+
+    if (sizeArray2 > 0)         // проверка условия необходимости дальнейшего решения задачи
+    {
+        // 3 часть - создание второго массива со строками удовлетворяющими условиям задачи
+        int count = 0;
+        var StringArray2 = new string[sizeArray2];     // создаем второй массив
+        for (int i = 0; i < sizeArray; i++)
+        {
+            if (StringArray[i].Length <= 3)
+            {
+                StringArray2[count] = StringArray[i];
+                count++;
+            }
+        }
+
+        // 4 часть - вывод результата
+        Console.Write("Результат: ");
+        for (int i = 0; i < sizeArray2 - 1; i++)
+        {
+            Console.Write(StringArray2[i]);
+            Console.Write(", ");
+        }
+        Console.WriteLine(StringArray2[sizeArray2 - 1]);
+    }
+    else Console.WriteLine("В введенных данных нет строк удовлетворяющих условиям задачи");
 }
-Console.WriteLine(StringArray2[sizeArray2-1]);
-}
-else Console.Write("В введенных данных нет строк удовлетворяющих условиям задачи");
+else Console.WriteLine("Строки не введены");
